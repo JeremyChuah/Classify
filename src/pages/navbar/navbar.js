c826b8d53bd12d1580e513017de5881bf4db64ff
JeremyChuah <JeremyChuah15@gmail.com>
2022-07-27 17:21:05 -0500

    Update navbar.js

--- src/pages/navbar/navbar.js
+++ src/pages/navbar/navbar.js
@@ -5,27 +5,24 @@ import searchIcon from "./search.svg";
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
 function Navbar() {
-  const [toggleMenu, setToggleMenu] = useState(false)
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
-
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
 
   const toggleNav = () => {
-    setToggleMenu(!toggleMenu)
-  }
+    setToggleMenu(!toggleMenu);
+  };
 
   useEffect(() => {
-
     const changeWidth = () => {
       setScreenWidth(window.innerWidth);
-    }
+    };
 
-    window.addEventListener('resize', changeWidth)
+    window.addEventListener("resize", changeWidth);
 
     return () => {
-        window.removeEventListener('resize', changeWidth)
-    }
-
-  }, [])
+      window.removeEventListener("resize", changeWidth);
+    };
+  }, []);
 
   return (
     <div className="flex flex-row w-full justfy-center items-center mt-10">
@@ -36,38 +33,41 @@ function Navbar() {
           </Link>
         </div>
         {(toggleMenu || screenWidth > 500) && (
-        <ul className="menu-list sm:flex sm:flex-row mr-5 sm:mr-0 list">
-          <li className="items sm:px-7 md:px-16">
-            <Link to="/" className="hover:text-blue-700 hover:font-semibold">
-              Home
-            </Link>
-          </li>
-          <li className="items sm:px-7 md:px-16">
-            <Link
-              to="/ClassCatalog"
-              className="hover:text-blue-700 hover:font-semibold"
-            >
-              Find a Course
-            </Link>
-          </li>
-          <li className="items sm:px-7 md:px-16">
-            <Link
-              to="/CourseLoad"
-              className="hover:text-blue-700 hover:font-semibold"
-            >
-              Personal Course Load
-            </Link>
-          </li>
-          <li className="items sm:px-7 md:px-16">
-            <Link
-              to="/About"
-              className="hover:text-blue-700 hover:font-semibold"
-            >
-              About Us
-            </Link>
-          </li>
-        </ul>)}
-        <button onClick={toggleNav} className="btn">☰</button>
+          <ul className="menu-list sm:flex sm:flex-row mr-5 sm:mr-0 list">
+            <li className="items sm:px-7 md:px-16">
+              <Link to="/" className="hover:text-blue-700 hover:font-semibold">
+                Home
+              </Link>
+            </li>
+            <li className="items sm:px-7 md:px-16">
+              <Link
+                to="/ClassCatalog"
+                className="hover:text-blue-700 hover:font-semibold"
+              >
+                Find a Course
+              </Link>
+            </li>
+            <li className="items sm:px-7 md:px-16">
+              <Link
+                to="/CourseLoad"
+                className="hover:text-blue-700 hover:font-semibold"
+              >
+                Personal Course Load
+              </Link>
+            </li>
+            <li className="items sm:px-7 md:px-16">
+              <Link
+                to="/About"
+                className="hover:text-blue-700 hover:font-semibold"
+              >
+                About Us
+              </Link>
+            </li>
+          </ul>
+        )}
+        <button onClick={toggleNav} className="btn">
+          ☰
+        </button>
       </div>
     </div>
   );
