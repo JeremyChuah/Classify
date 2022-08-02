e35a11e115feaa3bb910fe16e1f333fa5900f297
Jay Bhalala <bhalalajay722@gmail.com>
2022-08-01 22:49:14 -0500

    about us styling

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,48 +1,63 @@
-import React, { useState, useEffect } from "react";
-import OurTeamProfile from "./OurTeamProfile";
+import React from "react";
+import "./About.css";
 import Jay from "./square.jpg";
 import Jeremy from "./1650497222877.jpg";
 import Faizan from "./sqqqq.jpg";
 import Jad from "./sq2.png";
 
-var profiles = [
-    {
-     img: Jad,   
-     name: 'Jad Bazian',
-     title: 'Project Lead/Business Lead',
-    },
-   
-    {
-     img: Jeremy,   
-     name: 'Jeremy Chuah',
-     title: 'Lead Engineering & Product',
-    },
-   
-    {
-     img: Jay,
-     name: 'Jay Bhalala',
-     title: 'Chief Operating Officer & Developer',
-    },
-    {
-     img: Faizan,   
-     name: 'Faizan Kalam',
-     title: 'Marketing Lead',
-    },
-   
-   ];
-
 function About() {
 
   return (
     <div>
-        <h5>Our Co-Founders</h5>
-        {profiles.map((profile, index)=>
-            <OurTeamProfile 
-            img={profile.img}
-            name = {profile.name}
-            title={profile.title}
-            key={index} /> 
-        )}
+      <div class="wrapper">
+        <h1 className="about-us-text4">Our Co-Founders</h1>
+        <div class="team">
+          <div class="team_member">
+            <div class="team_img">
+              <img
+                class="rounded-circle z-depth-2"
+                src={Jad}
+                alt="Team_image"
+              />
+            </div>
+            <h3>Jad Bazian</h3>
+            <p class="role">Project Lead/Business Lead</p>
+          </div>
+          <div class="team_member">
+            <div class="team_img">
+              <img
+                class="rounded-circle z-depth-2"
+                src={Jeremy}
+                alt="Team_image"
+              />
+            </div>
+            <h3>Jeremy Chuah</h3>
+            <p class="role">Lead Engineering & Product</p>
+          </div>
+          <div class="team_member">
+            <div class="team_img">
+              <img
+                class="rounded-circle z-depth-2"
+                src={Jay}
+                alt="Team_image"
+              />
+            </div>
+            <h3>Jay Bhalala</h3>
+            <p class="role">Chief Operating Officer & Developer</p>
+          </div>
+          <div class="team_member">
+            <div class="team_img">
+              <img
+                class="rounded-circle z-depth-2"
+                src={Faizan}
+                alt="Team_image"
+              />
+            </div>
+            <h3>Faizan Kalam</h3>
+            <p class="role">Chief Marketing Officer</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
